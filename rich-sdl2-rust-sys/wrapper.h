5ca05116a66209b45c8e0c48077af9f06509af9a
Toshiharu Moriyama <aqua.bindi@gmail.com>
2023-01-23 22:31:37 +0900

    Revert include path

--- rich-sdl2-rust-sys/wrapper.h
+++ rich-sdl2-rust-sys/wrapper.h
@@ -1,12 +1,12 @@
 #define SDL_MAIN_HANDLED
-#include <SDL.h>
-#include <SDL_syswm.h>
-#include <SDL_vulkan.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+#include <SDL2/SDL_vulkan.h>
 
 #ifdef RICH_SDL2_RUST_TTF
-#include <SDL_ttf.h>
+#include <SDL2/SDL_ttf.h>
 #endif
 
 #ifdef RICH_SDL2_RUST_MIXER
-#include <SDL_mixer.h>
+#include <SDL2/SDL_mixer.h>
 #endif
